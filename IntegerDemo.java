87113ce36aa73385b8303e9c3b894cb13720fd5d
Priya2797 <34308150+Priya2797@users.noreply.github.com>
2018-01-04 16:02:46 +0530

    Add files via upload

---
+++ IntegerDemo.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+class IntegerDemo
+
+{
+
+public static void main(String args[])
+
+	{
+
+System.out.println("Enter the number:");
+
+Scanner sc = new Scanner(System.in);
+int num=sc.nextInt();
+	if(num <0)
+
+	{
+	System.out.println("NEGATIVE");
+
+	}
+	else if(num>0)
+
+	{
+	System.out.println("POSITIVE");
+	}
+
+
+	else
+
+	{
+	System.out.println("ZERO");
+
+	}
+	}
+
+}
